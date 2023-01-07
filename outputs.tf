2b35543ece317bd6892b72ae5710d329a895c290
lee-vincent <vinnie@vinnielee.io>
2023-01-07 17:46:48 -0500

    update versions

--- outputs.tf
+++ outputs.tf
@@ -16,35 +16,3 @@ output "rubrik_ips" {
 output "rubrik_bucket" {
   value = module.rubrik-cloud-cluster.backup_bucket_name
 }
-
-#Outputs for CBS
-output "cbs_gui_endpoint" {
-  value = cbs_array_aws.cbs_aws.gui_endpoint
-}
-output "cbs_repl_endpoint_ct0" {
-  value = cbs_array_aws.cbs_aws.replication_endpoint_ct0
-}
-output "cbs_floating_mgmt_ip" {
-  value = cbs_array_aws.cbs_aws.management_endpoint
-}
-output "cbs_repl_endpoint_ct1" {
-  value = cbs_array_aws.cbs_aws.replication_endpoint_ct1
-}
-output "cbs_iscsi_endpoint_ct0" {
-  value = cbs_array_aws.cbs_aws.iscsi_endpoint_ct0
-}
-output "cbs_iscsi_endpoint_ct1" {
-  value = cbs_array_aws.cbs_aws.iscsi_endpoint_ct1
-}
-
-
-
-
-output "secure_copy_configure_sh_command" {
-  value = format("%s%s%s%s%s", "scp -i $HOME/.ssh/bilh-aws-demo-master-key -oStrictHostKeyChecking=no ", "${local_file.configure_sh.filename}", " ec2-user@", "${aws_instance.bastion_instance.public_ip}", ":/home/ec2-user/")
-}
-
-# use this output to set up an ssh tunnel to the Pure and Rubrik management GUIs
-output "ssh_local_port_forwarding_command" {
-  value = format("%s%s%s%s%s%s%s", "ssh -N -i $HOME/.ssh/bilh-aws-demo-master-key", " -L 8443:", "${cbs_array_aws.cbs_aws.management_endpoint}", ":443 -L 8444:", "${module.rubrik-cloud-cluster.rubrik_cloud_cluster_ip_addrs[0]}", ":443 -oStrictHostKeyChecking=no -p 22 ec2-user@", "${aws_instance.bastion_instance.public_ip}")
-}
--- versions.tf
+++ versions.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    cbs = {
-      source  = "PureStorage-OpenConnect/cbs"
-      version = "0.6.0"
+      version = "4.49.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -14,17 +10,7 @@ terraform {
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = "~> 3.2.1"
     }
-    # manually download rubrik terraform provider from github
-    # https://github.com/rubrikinc/terraform-provider-rubrik/releases
-    # using version 1.0.4
-    # rename executable to terraform-provider-rubrik
-    # move to $HOME/.terraform.d/plugins/github.com/rubrikinc/rubrik/1.0.4/linux_amd64/
-    # add required_providers rubrik block to .terraform/modules/rubrik-cloud-cluster/main.tf
-    # rubrik = {
-    #   source  = "github.com/rubrikinc/rubrik"
-    #   version = "= 1.0.4"
-    # }
   }
 }
