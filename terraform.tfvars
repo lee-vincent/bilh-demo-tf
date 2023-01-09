d6527bd0844568f6b2da568b9ee0d1f759b8895f
lee-vincent <vinnie@vinnielee.io>
2023-01-09 14:01:33 -0500

    add back the template

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,12 +1,9 @@
-# See blog at https://davidstamen.com/2021/07/26/pure-cloud-block-store-on-aws-jump-start/ for more information on AWS Jump Start
-
 # AWS Variables
-aws_prefix  = "tf-"
-aws_region  = "us-east-1"
-aws_profile = "iamadmin-bilh-tf"
-# If multiple private subnets are used for Cloud Block Store, they must be all in the same Availability zone.
+aws_profile                   = "iamadmin-bilh-tf"
+aws_region                    = "us-east-1"
 aws_zone                      = "a"
-aws_bastion_instance_type     = "t3.large"
+aws_prefix                    = "tf-"
+wordpress_instance_type       = "t3.large"
 bilh_aws_demo_master_key_name = "bilh-aws-demo-master-key"
 
 ######################################
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "aws_profile" {
+  type        = string
+  description = "AWS iam user profile terraform will use to deploy"
+}
 variable "aws_region" {
   type    = string
   default = "us-east-1"
@@ -11,9 +15,10 @@ variable "aws_prefix" {
   description = "prefix added to aws tags"
   default     = "tf-"
 }
-variable "aws_bastion_instance_type" {
+variable "wordpress_instance_type" {
   type        = string
   description = "ec2 instance type"
+  default     = "t3.large"
 }
 variable "bilh_aws_demo_master_key_name" {
   type = string
@@ -21,10 +26,6 @@ variable "bilh_aws_demo_master_key_name" {
 variable "bilh_aws_demo_master_key_pub" {
   type      = string
   sensitive = true
-}
-variable "aws_profile" {
-  type        = string
-  description = "AWS profile terraform will use to deploy"
 }
 variable "bilh_aws_demo_master_key" {
   type = string
