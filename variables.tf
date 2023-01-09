98680fe7e46a283656c6630b276c9c40262435d2
lee-vincent <vinnie@vinnielee.io>
2023-01-09 11:44:40 -0500

    add variable descriptions and defaults

--- variables.tf
+++ variables.tf
@@ -1,5 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "aws_zone" {
+  type    = string
+  default = "a"
+}
+variable "aws_prefix" {
+  type        = string
+  description = "prefix added to aws tags"
+  default     = "tf-"
+}
 variable "aws_bastion_instance_type" {
-  type = string
+  type        = string
+  description = "ec2 instance type"
 }
 variable "bilh_aws_demo_master_key_name" {
   type = string
@@ -10,17 +24,8 @@ variable "bilh_aws_demo_master_key_pub" {
 }
 variable "aws_profile" {
   type        = string
-  description = "AWS profile."
+  description = "AWS profile terraform will use to deploy"
 }
 variable "bilh_aws_demo_master_key" {
-  type = string
-}
-variable "aws_region" {
-  type = string
-}
-variable "aws_zone" {
-  type = string
-}
-variable "aws_prefix" {
   type = string
 }
