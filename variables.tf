33d757ee0fee8f851336f60500e99951e6bfecad
lee-vincent <vinnie@vinnielee.io>
2023-01-13 04:12:48 -0500

    formatting

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_profile" {
 variable "environment" {
   type        = string
   description = "Infrastructure environment - dev, test, prod, etc."
-  default = "prod"
+  default     = "prod"
 }
 variable "aws_region" {
   type    = string
@@ -27,19 +27,19 @@ variable "wordpress_instance_type" {
   default     = "t3.large"
 }
 variable "ssh_key_name" {
-  type = string
+  type    = string
   default = "bilh-aws-demo-master-key"
 }
 variable "ssh_key_pub" {
-  type      = string
+  type        = string
   description = "public key material used to authenticate ssh connections to wordpress ec2 instance"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWqTspxyMXtEnJsoWXI1IFIxZH5xQ+AaJgbzd/JM6Tyt3vnrNzRQzAYwBruThfL/dCBVcZ6md3nV8BBbjP7SWTxA4V5+c5PyI/A2wDnKYZ3j+rEIoiK3dVcrOTC2PCfladVUHZ9AHayQF+QJZTF0uPWuON5wXPb7t/zCf1/TWxvwyD2OJ1N5UZ1JBvC0XmJyT1vSMLDjHjIr3TfqCrKjODC97fDw9hxJ8061043rAf+13nYmX95H8JYj5lfkB0rBOcWaGplALSz7D6Y3O3OTWvdFciw4mZ9/AaESDtlVbjUOOgmTHxI5579ob1x9BCZfdCw4NFj+r4BZqjheKcyQJ"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWqTspxyMXtEnJsoWXI1IFIxZH5xQ+AaJgbzd/JM6Tyt3vnrNzRQzAYwBruThfL/dCBVcZ6md3nV8BBbjP7SWTxA4V5+c5PyI/A2wDnKYZ3j+rEIoiK3dVcrOTC2PCfladVUHZ9AHayQF+QJZTF0uPWuON5wXPb7t/zCf1/TWxvwyD2OJ1N5UZ1JBvC0XmJyT1vSMLDjHjIr3TfqCrKjODC97fDw9hxJ8061043rAf+13nYmX95H8JYj5lfkB0rBOcWaGplALSz7D6Y3O3OTWvdFciw4mZ9/AaESDtlVbjUOOgmTHxI5579ob1x9BCZfdCw4NFj+r4BZqjheKcyQJ"
 }
 # IP Address of SSH Client
 variable "workstation_cidr" {
-  type = string
+  type        = string
   description = "public ip address of ssh client that will be used in security groups providing access to wordpress ec2 instance"
-  default = "0.0.0.0/0"
+  default     = "0.0.0.0/0"
 }
 # Wordpress Variables
 variable "wp_db_name" {
